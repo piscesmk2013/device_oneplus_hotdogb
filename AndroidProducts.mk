992cbefed9baabceb251a8a0aeecdd7b14828120
piscesmk2013 <piscesmk2013@gmail.com>
2024-06-13 21:53:18 +0800

    hotdogb: Initial PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,9 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2024 The PixelOS AOSP
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_hotdogb.mk
+    $(LOCAL_DIR)/aosp_hotdogb.mk
